eb4b84332377f4739f905e0a40c79233650305ae
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-08-09 10:10:48 +0200

    modify endpoints

---
+++ routes/employees.routes.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const ObjectId = require('mongodb').ObjectId;
+
+router.get('/employees', (req, res) => {
+  req.db.collection('employees')
+    .find()
+    .toArray()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
+
+router.get('/employees/random', (req, res) => {
+  req.db.collection('employees')
+    .aggregate([{ $sample: { size: 1 } }])
+    .toArray()
+    .then((data) => {
+      res.json(data[0]);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
+
+router.get('/employees/:id', (req, res) => {
+  req.db.collection('employees')
+    .findOne({ _id: ObjectId(req.params.id) })
+    .then((data) => {
+      if(!data) res.status(404).json({ message: 'Not found' });
+      else res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
+
+router.post('/employees', (req, res) => {
+  const { firstName, lastName, department } = req.body;
+
+  req.db.collection('employees')
+    .insertOne({ firstName: firstName, lastName: lastName, department: department})
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message:err });
+    })
+});
+
+router.put('/employees/:id', (req, res) => {
+  const { firstName, lastName, department } = req.body;
+
+  req.db.collection('employees')
+    .updateOne({ _id: ObjectId(req.params.id) }, { $set: { firstName: firstName, lastName: lastName, department: department }})
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    })
+});
+
+router.delete('/employees/:id', (req, res) => {
+  req.db.collection('employees')
+    .deleteOne({ _id: ObjectId(req.params.id) })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    })
+});
+
+module.exports = router;
